e0058ce07ca2d903e2b50cfd9abaee6a4616159b
maryamnmkfrsh <113207833+maryamnmkfrsh@users.noreply.github.com>
2023-11-10 19:51:05 +0000

    updated spotify player

--- src/components/sidebar/SpotifyPlayer.js
+++ src/components/sidebar/SpotifyPlayer.js
@@ -1,12 +1,16 @@
 const SpotifyPlayer = () => {
     return (<div>
-        <iframe style="border-radius:12px" 
-        src="https://open.spotify.com/embed/playlist/6Jehxo4XAdg1hEC8SWYJnz?utm_source=generator" 
-        width="100%" height="352" frameBorder="0" 
-        allowfullscreen="" 
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-        loading="lazy">
-        </iframe>
+       <iframe
+                style={{ borderRadius: '12px' }}
+                src="https://open.spotify.com/embed/playlist/6Jehxo4XAdg1hEC8SWYJnz?utm_source=generator"
+                width="100%"
+                height="352"
+                frameBorder="0"
+                allowFullScreen=""
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+            >
+            </iframe>
     </div>)
 }
 
