2e354da1b2cf11b1f1bf3a6db166b4b57d5aed59
Vivek Selvaraj <vive@umich.edu>
2023-12-06 23:09:32 -0500

    merge from main

--- src/MomentumMate.js
+++ src/MomentumMate.js
@@ -7,14 +7,69 @@ import RightSideBar from './components/RightSideBar';
 import { useEffect, useState } from 'react';
 import TestFirebaseModule from './components/TestFirebaseModule';
 
-function MomentumMate({user}) {
-  const [tasks, setTasks] = useState([]);
+const getHeaderDate = () => {
+  const today = new Date();
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const shortMonth = [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ];
+  return (
+      <span>
+         Today is {days[today.getDay()]}, {shortMonth[today.getMonth()]} {today.getDate()}
+      </span>
+  )
+}
 
-  const addTask = (item) => {
-    setTasks(previousValue => {
-      return [...previousValue, {name: item}]
+function MomentumMate() {
+  const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [taskTimer, setTaskTimer] = useState(null);
+  const [currentTask, setCurrentTask] = useState(null);
+
+  useEffect(() => {
+    if (tasks.length === 0) return; 
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks]);
+
+  useEffect (() => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTasks(storedTasks)
+  }, []);
+
+  const handleToggle = (taskIndex, updateCompleted) => {
+    setTasks((previousValue) => {
+      const newTasks = [...previousValue]
+      newTasks[taskIndex].completed = updateCompleted
+      return newTasks; 
     })
   }
+
+  const addTask = (item, time) => {
+    setTasks(previousValue => {
+      return [...previousValue, { id: Date.now(), task: item, time: time, completed: false }]
+    })
+  }
+
+  const startTask = (task) => {
+    setSelectedTask(task);
+    setCurrentTask(task.task);
+    setTaskTimer(setInterval(() => {
+      console.log(`Task in progress: ${task.task} - ${task.time}`);
+    }, 1000));
+  };
+
+  const pauseTask = () => {
+    clearInterval(taskTimer);
+    console.log(`Task paused: ${selectedTask.task}`);
+  };
+
+  const stopTask = () => {
+    clearInterval(taskTimer);
+    setSelectedTask(null);
+    setCurrentTask(null);
+  };
 
   return <>
     <div style={{height: '100vh'}}>
@@ -23,12 +78,16 @@ function MomentumMate({user}) {
       </header>
       <main>
         <div>
-          <CurrentTask />
-          <TaskList className="my-2 task-list" onAdd={{addTask}}/>
+          <CurrentTask 
+          currentTask={currentTask}
+          selectedTask={selectedTask}
+          onStart={startTask}
+          onPause={pauseTask}
+          onStop={stopTask}/>
+          <TaskList tasks={tasks} startTask={startTask} setCurrentTask={setCurrentTask}/>
         </div>
         <div className='ms-2 p-0'>
-          <TestFirebaseModule user={user} />
-          {/* <RightSideBar className="sidebar" /> */}
+          <RightSideBar className="sidebar" />
         </div>
       </main>
     </div>
--- src/components/TaskAdd.js
+++ src/components/TaskAdd.js
@@ -1,31 +1,67 @@
-// component used to add new task to task list 
-
-//still working on this
-
 import { useState } from "react";
+import { TextField, Button } from '@mui/material';
 
-const TaskAdd = ({TaskAdd}) => {
-    const [input, setInput]=useState(''); 
+const TaskAdd = ({ addTask }) => {
+    const [task, setTask] = useState('');
+    const [hours, setHours] = useState('');
+    const [minutes, setMinutes] = useState('');
 
-    function submitHandler(e) {
-        e.preventDefault(); 
-        // onAdd(input)
-        setInput("")
-    }
+
+    const formatTime = (inputHours, inputMinutes, inputSeconds) => {
+        const formattedHours = inputHours < 10 ? `0${inputHours}` : `${inputHours}`;
+        const formattedMinutes = inputMinutes < 10 ? `0${inputMinutes}` : `${inputMinutes}`;
+        return `${formattedHours}:${formattedMinutes}`;
+      };
+    
+    
+      const handleTimeChange = (setValue, e) => {
+        const input = e.target.value;
+        const regex = /^\d*$/;
+    
+        if (regex.test(input) || input === "") {
+          const inputValue = input === "" ? 0 : parseInt(input, 10);
+          setValue(inputValue);
+        }
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        const formattedTime = formatTime(hours, minutes);
+        addTask({ task, time: formattedTime });
+        setTask('');
+        setHours('');
+        setMinutes('');
+      };
 
     return (
         <>
-        <form onSubmit={(e) => submitHandler}>
-            <input
-            type="text"
-            placeholder="Add a new task..."
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            />
+        <div className='d-flex gap-3'>
+            <TextField 
+            fullWidth 
+            value={task} 
+            onChange={(e) => setTask(e.target.value)}
+            label="Add a new task" 
+            variant="outlined" />
+
+            <TextField
+            label="Hours"
+            value={hours}
+            onChange={(e) => handleTimeChange(setHours, e)}
+            variant="outlined" />
             
-            <button> + </button>
+            <TextField
+            label="Minutes"
+            value={minutes}
+            onChange={(e) => handleTimeChange(setMinutes, e)}
+            variant="outlined"/>
             
-        </form>
+            <Button
+            className='flex-grow-1'
+            onClick={handleSubmit}
+            variant="contained">
+            Add
+            </Button>
+        </div>
         </>
     )
 }
